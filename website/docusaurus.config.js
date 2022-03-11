b4f03e95330620d23b589a977824f96e9c6e5784
Edge <69719569+omxpro@users.noreply.github.com>
2022-03-11 20:43:22 +0530

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://dbh-docs.github.io',
-  baseUrl: '/tests',
+  baseUrl: '/tests/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
